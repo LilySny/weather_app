2c2e549806d8b1b242714cf0692975d1963e84d2
LilySny <liviacastilholi@gmail.com>
2021-07-28 22:59:46 -0300

    added secure to url

--- lib/weather_forecast/utils/utils.dart
+++ lib/weather_forecast/utils/utils.dart
@@ -1,6 +1,6 @@
 String weatherBaseUrl(String endpoint, String city, String apiKey,
     {String unit = 'metric'}) {
-  return 'http://api.openweathermap.org/data/2.5/$endpoint?units=$unit&q=$city&appid=$apiKey';
+  return 'https://api.openweathermap.org/data/2.5/$endpoint?units=$unit&q=$city&appid=$apiKey';
 }
 
 const apiKey = 'a5048a96bc9960c6799fb0d18d242aff';
